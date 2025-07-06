2d243c3685f3dff5ff6444d3d1dab356f46a53ab
Shivam <shiwamsingh50@gmail.com>
2025-07-06 15:20:57 +0530

    Update SignIn component styling and structure; add SignIn.css for improved layout

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import { useNavigate } from "react-router-dom";
 import SignInButton from "../../utils/buttons/SignInButton";
-import "../welcome/WelcomePage.css";
 import SignUpButton from "../../utils/buttons/SignUpButton";
+import "./SignIn.css";
 
 function SignIn() {
   const [isVisible, setIsVisible] = useState(true);
@@ -15,32 +15,34 @@ function SignIn() {
     setIsVisible(!isVisible);
   };
   return (
-    <div className="signin__floatContainer">
-      <div className="signin__closeButton" onClick={handleClose}>
-        <CloseIcon onClick={handleClose} />
-      </div>
-      {isVisible ? (
-        <div className="signin__mainContainer">
-          <div className="signin__logoContainer">
-            <XIcon />
-          </div>
-          <div className="signIn__header">
-            <h2>Sign in to X</h2>
-          </div>
-          <div className="signIn__formContainer">
-            <form className="signIn__form">
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" required />
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" name="password" required />
-              <SignUpButton text="Next" flow="true" />
-              <SignInButton text="Forgot password?" />
-            </form>
-          </div>
+    <div className="mainContainer">
+      <div className="signin__floatContainer">
+        <div className="signin__closeButton" onClick={handleClose}>
+          <CloseIcon onClick={handleClose} />
         </div>
-      ) : (
-        navigate("/")
-      )}
+        {isVisible ? (
+          <div className="signin__mainContainer">
+            <div className="signin__logoContainer">
+              <XIcon fontSize="inherit" />
+            </div>
+            <div className="signIn__headerFlow">
+              <h1>Sign in to X</h1>
+            </div>
+            <div className="signIn__formContainer">
+              <form className="signIn__form">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" required />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" required />
+                <SignUpButton text="Next" flow="true" />
+                <SignInButton text="Forgot password?" />
+              </form>
+            </div>
+          </div>
+        ) : (
+          navigate("/")
+        )}
+      </div>
     </div>
   );
 }
